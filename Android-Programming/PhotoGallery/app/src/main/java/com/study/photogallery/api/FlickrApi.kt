d1b84768d8f722490882b7fcc7ad5bd2dabbfc37
ChangXXX <eqqt97@naver.com>
2022-08-12 21:14:02 +0900

    thread lifeycle 인식하기

--- Android-Programming/PhotoGallery/app/src/main/java/com/study/photogallery/api/FlickrApi.kt
+++ Android-Programming/PhotoGallery/app/src/main/java/com/study/photogallery/api/FlickrApi.kt
@@ -1,15 +1,20 @@
 package com.study.photogallery.api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
     @GET(
         "services/rest/?method=flickr.interestingness.getList" +
-            "&api_key=myAPI" +
+            "&api_key=dc9aaee1f4c0b0b80e41b1619ccfd59d" +
             "&format=json" +
             "&nojsoncallback=1" +
             "&extras=url_s"
     )
     fun fetchPhotos(): Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
