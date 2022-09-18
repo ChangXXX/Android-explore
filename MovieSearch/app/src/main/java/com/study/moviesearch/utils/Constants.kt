0256f626af41fd257d01bcdab9c05d1dfdcb4d7c
ChangXXX <eqqt97@naver.com>
2022-09-19 02:02:45 +0900

    refactor: remove api key

--- MovieSearch/app/src/main/java/com/study/moviesearch/utils/Constants.kt
+++ MovieSearch/app/src/main/java/com/study/moviesearch/utils/Constants.kt
@@ -6,8 +6,8 @@ object Constants {
      */
     const val BASE = "https://openapi.naver.com/v1/"
     const val MOVIES = "movie.json"
-    const val CLIENT_ID = "LbUG25DMP4Ewn2l8GRQV"
-    const val CLIENT_SECRET = "3C1Ngwi_rq"
+    const val CLIENT_ID = "ID"
+    const val CLIENT_SECRET = "SECRET"
 
     /**
      * Database 관련
