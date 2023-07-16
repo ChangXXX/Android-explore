bc2a4662009466b5a8fad9fc3a0f1d7df844c3d3
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-07-16 21:28:54 +0900

    Barrier 테스트

--- ComposeConstraint/app/src/main/java/com/example/composeconstraint/MainActivity.kt
+++ ComposeConstraint/app/src/main/java/com/example/composeconstraint/MainActivity.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.composeconstraint.ui.theme.ComposeConstraintTheme
 
@@ -40,38 +43,14 @@ fun ConstraintLayoutEx() {
         modifier = Modifier
             .fillMaxSize(),
     ) {
-        val (redBox, magentaBox, greenBox, yellowBox) = createRefs()
-
+        val (redBox, magentaBox, yellowBox, text) = createRefs()
+        val boxs = arrayOf(redBox, yellowBox, magentaBox)
         Box(
             modifier = Modifier
                 .size(40.dp)
                 .background(Color.Red)
                 .constrainAs(redBox) {
-                    bottom.linkTo(anchor = parent.bottom, margin = 8.dp)
-                    end.linkTo(anchor = parent.end, margin = 4.dp)
-                },
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Magenta)
-                .constrainAs(magentaBox) {
-                    bottom.linkTo(anchor = redBox.top, margin = 8.dp)
-//                    start.linkTo(parent.start)
-//                    end.linkTo(parent.end)
-                    centerHorizontallyTo(parent)
-                },
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Green)
-                .constrainAs(greenBox) {
-//                    start.linkTo(parent.start)
-//                    end.linkTo(parent.end)
-//                    top.linkTo(parent.top)
-//                    bottom.linkTo(parent.bottom)
-                    centerTo(parent)
+                    top.linkTo(parent.top, margin = 18.dp)
                 },
         )
         Box(
@@ -79,8 +58,33 @@ fun ConstraintLayoutEx() {
                 .size(40.dp)
                 .background(Color.Yellow)
                 .constrainAs(yellowBox) {
-                    start.linkTo(anchor = greenBox.end)
-                    bottom.linkTo(anchor = greenBox.top)
+                    top.linkTo(parent.top, margin = 26.dp)
+                },
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Magenta)
+                .constrainAs(magentaBox) {
+                    top.linkTo(parent.top, margin = 34.dp)
+                },
+        )
+
+        createHorizontalChain(
+            elements = boxs,
+            chainStyle = ChainStyle.SpreadInside,
+        )
+        val barrier = createBottomBarrier(
+            elements = boxs,
+            margin = 10.dp,
+        )
+        Text(
+            text = "안녕하세요 텍스트인데요 배리어가 잘 동작하나 보려고 적어봤어요 이게 맞나요? 아무튼 이건 텍스트에요",
+            modifier = Modifier
+                .width(40.dp)
+                .constrainAs(text) {
+                    top.linkTo(barrier)
+                    start.linkTo(yellowBox.start)
                 },
         )
     }
