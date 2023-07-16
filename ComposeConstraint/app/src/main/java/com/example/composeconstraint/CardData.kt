23daa17da85684c0ce06cf67e98d1ed5db0c9d6a
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-07-16 21:51:11 +0900

    Constraint로 Card 만들어보기

---
+++ ComposeConstraint/app/src/main/java/com/example/composeconstraint/CardData.kt
@@ -0,0 +1,8 @@
+package com.example.composeconstraint
+
+data class CardData(
+    val imageUrl: String = "",
+    val imageDescription: String = "",
+    val author: String = "",
+    val description: String = ""
+)
--- ComposeConstraint/app/src/main/java/com/example/composeconstraint/MainActivity.kt
+++ ComposeConstraint/app/src/main/java/com/example/composeconstraint/MainActivity.kt
@@ -6,18 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImage
 import com.example.composeconstraint.ui.theme.ComposeConstraintTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +39,12 @@ class MainActivity : ComponentActivity() {
             ComposeConstraintTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(
+                            state = rememberScrollState(),
+                            enabled = true,
+                        ),
                     color = MaterialTheme.colors.background,
                 ) {
                     ConstraintLayoutEx()
@@ -90,10 +107,75 @@ fun ConstraintLayoutEx() {
     }
 }
 
+@Composable
+fun CardEx(cardData: CardData) {
+    val placeHolderColor = Color(0x33000000)
+    Card(
+        elevation = 8.dp,
+        modifier = Modifier.padding(6.dp),
+    ) {
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+        ) {
+            val (profileImage, author, description) = createRefs()
+            AsyncImage(
+                model = cardData.imageUrl,
+                contentDescription = cardData.imageDescription,
+                contentScale = ContentScale.Crop,
+                placeholder = ColorPainter(color = placeHolderColor),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(40.dp)
+                    .constrainAs(profileImage) {
+                        centerVerticallyTo(parent)
+                        start.linkTo(parent.start, margin = 8.dp)
+                    },
+            )
+            Text(
+                text = cardData.author,
+                modifier = Modifier.constrainAs(author) {
+                    linkTo(profileImage.end, parent.end, startMargin = 8.dp, endMargin = 8.dp)
+                    width = Dimension.fillToConstraints
+                },
+            )
+            Text(
+                text = cardData.description,
+                modifier = Modifier.constrainAs(description) {
+                    linkTo(profileImage.end, parent.end, startMargin = 8.dp, endMargin = 8.dp)
+                    width = Dimension.fillToConstraints
+                },
+                maxLines = 2,
+            )
+            val chain = createVerticalChain(author, description, chainStyle = ChainStyle.Packed)
+            constrain(chain) {
+                top.linkTo(parent.top, margin = 8.dp)
+                bottom.linkTo(parent.bottom, margin = 8.dp)
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun ConstraintLayoutExPreview() {
     ComposeConstraintTheme {
         ConstraintLayoutEx()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CardPreview() {
+    ComposeConstraintTheme() {
+        CardEx(
+            cardData = CardData(
+                "",
+                "hi??",
+                "작가입니다",
+                "글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요, 글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요",
+            ),
+        )
     }
 }
