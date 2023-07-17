2a0df0bda57c2a2da31d1325759cb65a1358050a
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-07-17 23:33:33 +0900

    snackbar

--- ComposeConstraint/app/src/main/java/com/example/composeconstraint/MainActivity.kt
+++ ComposeConstraint/app/src/main/java/com/example/composeconstraint/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.composeconstraint
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarDuration
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeconstraint.ui.theme.ComposeConstraintTheme
 import com.example.composeconstraint.ui.theme.components.canvas.CanvasEx
 import com.example.composeconstraint.ui.theme.components.card.CardEx
@@ -18,43 +35,80 @@ import com.example.composeconstraint.ui.theme.components.constraintlayoutex.Cons
 import com.example.composeconstraint.ui.theme.components.customdialog.CustomDialog
 import com.example.composeconstraint.ui.theme.components.dialog.DialogEx
 import com.example.composeconstraint.ui.theme.components.dropdown.DropDownEx
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeConstraintTheme {
-                // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .verticalScroll(
-                            state = rememberScrollState(),
-                            enabled = true,
-                        ),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                var counter by remember { mutableStateOf(0) }
+                val coroutineScope = rememberCoroutineScope()
+                val scaffoldState = rememberScaffoldState()
+
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("그냥 이것저것")
+                            },
+                        )
+                    },
                 ) {
-                    ConstraintLayoutEx()
-                    CardEx(
-                        CardData(
-                            "",
-                            "hi??",
-                            "작가입니다",
-                            "글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요, 글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요",
-                        ),
-                    )
-                    CardEx(
-                        CardData(
-                            "",
-                            "hi??",
-                            "작가입니다22",
-                            "글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요, 글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요",
-                        ),
-                    )
-                    CanvasEx()
-                    DialogEx()
-                    CustomDialog()
-                    DropDownEx()
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(it)
+                            .verticalScroll(state = rememberScrollState()),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        ConstraintLayoutEx()
+                        CardEx(
+                            CardData(
+                                "",
+                                "hi??",
+                                "작가입니다",
+                                "글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요, 글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요",
+                            ),
+                        )
+                        CardEx(
+                            CardData(
+                                "",
+                                "hi??",
+                                "작가입니다22",
+                                "글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요, 글의 내용인데 이게 길면 어떻게 되나 보려고 길게 한번 써보고 있어요",
+                            ),
+                        )
+                        CanvasEx()
+                        DialogEx()
+                        CustomDialog()
+                        DropDownEx()
+                        Button(
+                            onClick = {
+                                counter++
+                                coroutineScope.launch {
+                                    val result = scaffoldState.snackbarHostState.showSnackbar(
+                                        message = "카운터에요 $counter",
+                                        actionLabel = "닫기",
+                                        duration = SnackbarDuration.Short,
+                                    )
+                                    when (result) {
+                                        SnackbarResult.Dismissed -> {
+                                            Log.d("SnackbarResult", "Dismissed")
+                                        }
+                                        SnackbarResult.ActionPerformed -> {
+                                            Log.d("SnackbarResult", "ActionPerformed")
+                                        }
+                                    }
+                                }
+                            },
+                            modifier = Modifier.width(200.dp)
+                                .height(50.dp),
+                        ) {
+                            Text("더하기")
+                        }
+                    }
                 }
             }
         }
