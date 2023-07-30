e3597eede7f5e7b2f8fa5b9ced2822dc9330242a
ckdals0439@flow.team <53431177+ChangXXX@users.noreply.github.com>
2023-07-30 22:20:25 +0900

    CompositionLocal 사용 - 암시적이라 파라미터로 넘기는게 더 좋을듯

--- CompositionLocal/app/src/main/java/com/example/compositionlocal/MainActivity.kt
+++ CompositionLocal/app/src/main/java/com/example/compositionlocal/MainActivity.kt
@@ -7,11 +7,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compositionlocal.ui.theme.CompositionLocalTheme
@@ -33,16 +39,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val LocalElevation = compositionLocalOf { 8.dp }
+
 @Composable
 fun Greeting(name: String) {
-    Card(
-        modifier = Modifier.padding(8.dp),
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
+    CompositionLocalProvider(LocalElevation provides 12.dp) {
+        Card(
+            modifier = Modifier.padding(8.dp),
+            elevation = LocalElevation.current,
         ) {
-            for (i in 0 until 9) {
-                Text(shiftRightText(name, name.length - i))
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                ) {
+                    for (i in 0 until 9) {
+                        if (i in 2..6) {
+                            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+                                Text(shiftRightText(name, name.length - i))
+                            }
+                        } else if (i in 7..8) {
+                            CompositionLocalProvider(LocalContentColor provides Color.Magenta) {
+                                Text(shiftRightText(name, name.length - i))
+                                Text("${LocalContentColor.current}")
+                            }
+                        } else {
+                            Text(shiftRightText(name, name.length - i))
+                            Text("${LocalContentColor.current}")
+                        }
+                    }
+                }
             }
         }
     }
