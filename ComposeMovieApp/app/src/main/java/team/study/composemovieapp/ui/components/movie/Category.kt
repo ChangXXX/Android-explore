bffcd163127272867f859bb419eb85c7e659159b
changXXX <53431177+ChangXXX@users.noreply.github.com>
2024-05-16 20:43:56 +0900

    Category Item 추가

---
+++ ComposeMovieApp/app/src/main/java/team/study/composemovieapp/ui/components/movie/Category.kt
@@ -0,0 +1,46 @@
+package team.study.composemovieapp.ui.components.movie
+
+import MovieItem
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CategoryRow() {
+    Column(
+
+    ) {
+        CategoryTitle("Action")
+        LazyRow(
+            contentPadding = PaddingValues(
+                horizontal = 10.dp
+            )
+        ) {
+//            itemsIndexed
+            item {
+                MovieItem()
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryTitle(titleName: String) {
+    Text(
+        text = titleName,
+        modifier = Modifier
+            .padding(10.dp)
+    )
+}
+
+@Preview
+@Composable
+fun CategoryRowPreview() {
+    CategoryRow()
+}
