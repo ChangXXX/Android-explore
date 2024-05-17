2feef7f0eb8c80096f1fa06d12e47ed11d13ad02
changXXX <53431177+ChangXXX@users.noreply.github.com>
2024-05-17 20:58:41 +0900

    테마 적용

--- ComposeMovieApp/app/src/main/java/team/study/composemovieapp/ui/components/movie/Category.kt
+++ ComposeMovieApp/app/src/main/java/team/study/composemovieapp/ui/components/movie/Category.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import team.study.composemovieapp.ui.theme.ComposeMovieAppTheme
+import team.study.composemovieapp.ui.theme.Paddings
 
 @Composable
 fun CategoryRow() {
@@ -35,12 +38,18 @@ fun CategoryTitle(titleName: String) {
     Text(
         text = titleName,
         modifier = Modifier
-            .padding(10.dp)
+            .padding(
+                vertical = Paddings.large,
+                horizontal = Paddings.extra
+            ),
+        style = MaterialTheme.typography.headlineMedium
     )
 }
 
 @Preview
 @Composable
 fun CategoryRowPreview() {
-    CategoryRow()
+    ComposeMovieAppTheme {
+        CategoryRow()
+    }
 }
