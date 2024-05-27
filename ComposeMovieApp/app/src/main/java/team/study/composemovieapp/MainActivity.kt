6d2a7a1b5267045591abba3b00c0560e9e833d2d
changXXX <53431177+ChangXXX@users.noreply.github.com>
2024-05-28 00:38:49 +0900

    setting : navigation

--- ComposeMovieApp/app/src/main/java/team/study/composemovieapp/MainActivity.kt
+++ ComposeMovieApp/app/src/main/java/team/study/composemovieapp/MainActivity.kt
@@ -1,47 +1,18 @@
 package team.study.composemovieapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import team.study.composemovieapp.ui.theme.ComposeMovieAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ComposeMovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeMovieAppTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
---
+++ ComposeMovieApp/app/src/main/java/team/study/composemovieapp/features/detail/DetailFragment.kt
@@ -0,0 +1,23 @@
+package team.study.composemovieapp.features.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import team.study.composemovieapp.ui.theme.ComposeMovieAppTheme
+
+class DetailFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                ComposeMovieAppTheme {
+                    Text(text = "DetailFragment")
+                }
+            }
+        }
+    }
+}
