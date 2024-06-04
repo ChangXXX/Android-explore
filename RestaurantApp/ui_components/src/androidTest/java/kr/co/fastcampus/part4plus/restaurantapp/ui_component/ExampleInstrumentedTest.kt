e9a04e474ffbe030bed906e2285274fc8d209df9
changXXX <53431177+ChangXXX@users.noreply.github.com>
2024-06-04 22:34:53 +0900

    RestaurantApp

--- RestaurantApp/ui_components/src/androidTest/java/kr/co/fastcampus/part4plus/restaurantapp/ui_component/ExampleInstrumentedTest.kt
+++ RestaurantApp/ui_components/src/androidTest/java/kr/co/fastcampus/part4plus/restaurantapp/ui_component/ExampleInstrumentedTest.kt
@@ -19,6 +19,9 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("kr.co.fastcampus.part4plus.restaurantapp.ui_component", appContext.packageName)
+        assertEquals(
+            "kr.co.fastcampus.part4plus.restaurantapp.ui_component",
+            appContext.packageName
+        )
     }
 }
